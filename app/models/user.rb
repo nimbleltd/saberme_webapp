e2cb03d7905b02a412d6a44bfd51a52c470623fa
Paul Campbell <nimbleltd@gmail.com>
2013-06-23 16:57:09 -0500

    drop down menu for assigning job to other employees

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :videos, :foreign_key => "customer_id"
   has_many :assigned_videos, :class_name => "Video", :foreign_key => "employee_id", :conditions => "videos.state != 'approved'"
 
+  scope :employee, -> {where :employee => true}
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -11,6 +13,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  # def self.employee
+  #   where(:employee => true)
+  # end
 
   def customer?
     !employee?
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,11 @@ employee = User.new :email => "bob@gmail.com", :password => "asdfasdf", :passwor
 employee.employee = true
 employee.save!
 
+employee = User.new :email => "sally@sally.com", :password => "asdfasdf", :password_confirmation => "asdfasdf"
+employee.employee = true
+employee.save!
+
+
 Video.create! :title => "Jay Jamming", :youtube_url => "http://www.youtube.com/watch?v=vSOYE8iLmPk", :state => "submitted", :customer_id => customer.id
 Video.create! :title => "Rockets with Glenn", :youtube_url => "http://www.youtube.com/watch?v=w3MPU7uf7l8", :state => "submitted", :customer_id => customer.id
-Video.create! :title => "Rockets with Glenn", :youtube_url => "http://www.youtube.com/watch?v=w3MPU7uf7l8", :state => "submitted", :customer_id => customer.id
+Video.create! :title => "Derby Race", :youtube_url => "http://www.youtube.com/watch?v=y44E6btjAkA", :state => "submitted", :customer_id => customer.id
