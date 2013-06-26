8619387026a77d5a20a5a1e395a1675a9de0eb3f
Paul Campbell <nimbleltd@gmail.com>
2013-06-26 10:31:38 -0500

    added secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,14 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Saberme::Application.config.secret_token = 'd029901d6521f12c478c3523d4c2115ec442bd2340c9b30b8d4247c6f009eb5c2cab55bcb67f6392b0d9c589be8efd5eb9b198885eb51767a015afdbfea28840'
+
+if Rails.env.development? or Rails.env.test?
+ Saberme::Application.config.secret_token = 'd029901d6521f12c478c3523d4c2115ec442bd2340c9b30b8d4247c6f009eb5c2cab55bcb67f6392b0d9c589be8efd5eb9b198885eb51767a015afdbfea28840'
+else
+    Saberme::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
+
+
+# use "rake secret" to generate a random token
+#Tell Heroku what your file is
+# heroku config:set SECRET_TOKEN = "ca9ebe7fa8ba1c92215f9fe618f621c813db60fca83948f9bc2e28589baf1c82a74413be11ed27bcafc574386f7324f2f8ac83fce128eb327563d64af35a4944"
